b23f24c6568af000946c0d6b187ddee2147db749
namluong <namggg462@gmail.com>
2020-11-13 09:16:01 +0700

    dev commit 2

--- data.js
+++ data.js
@@ -1,6 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const master = 10
 const master = 11
 =======
 const commit = 1
 >>>>>>> dev commit 1
+=======
+const commit = 1
+
+const commit = 2
+>>>>>>> dev commit 2
